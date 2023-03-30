ba6f45275518b5b815bf26a6151a19e8c2907f1c
jonathanwong110 <jonathan.wong110@gmail.com>
2023-03-30 15:21:17 -0400

    graph is now fully functioning. included function to update the graph with each new input

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+
+def update_figure(data):
+    axes = fig.axes
+    x = [i[0] for i in data]
+    y = [i[1] for i in data]
+    axes[0].plot(x, y, 'g-')
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack()
+
+
 sg.theme('DarkGrey4')
 table_contents = []
 layout = [
@@ -18,6 +28,12 @@ layout = [
 
 window = sg.Window('Graph App', layout, finalize=True)
 
+# matplotlib
+fig = matplotlib.figure.Figure(figsize=(5, 4))
+fig.add_subplot(111).plot([], [])
+figure_canvas_agg = FigureCanvasTkAgg(fig, window['-CANVAS-'].TKCanvas)
+figure_canvas_agg.draw()
+figure_canvas_agg.get_tk_widget().pack()
 
 while True:
     event, values = window.read()
@@ -29,5 +45,6 @@ while True:
             table_contents.append([len(table_contents) + 1, float(new_value)])
             window['-TABLE-'].update(table_contents)
             window['-INPUT-'].update('')
+            update_figure(table_contents)
 
 window.close()
